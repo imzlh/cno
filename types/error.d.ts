f7f037d0c852e6e7000b4309e3fc6db5e4bcb212
imzlh <2131601562@qq.com>
2025-12-06 16:41:46 +0800

    remove types/

--- types/error.d.ts
+++
@@ -1,8 +0,0 @@
-declare namespace CModuleError {
-    export class Error extends globalThis.Error {
-        /**
-         * ERRNO code, mostly for syscall results.
-         */
-        code: number;
-    }
-}
