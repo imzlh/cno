79af2c0a78ccecdae0449a073205b120038aae52
imzlh <2131601562@qq.com>
2026-05-03 10:34:09 +0800

    fix: circular dependent

--- src/node/fs/index.ts
+++ src/node/fs/index.ts
@@ -3,12 +3,6 @@
  * 基于 CModuleFS 和 CModuleAsyncFS 实现
  */
 
-export * from './constants';
-export * from './sync';
-export * from './callbacks';
 
-import * as promises from './promises';
-export { promises };
-
-import * as fs from './index';
-export default fs;
+export * as default from "./mod";
+export * from "./mod";
---
+++ src/node/fs/mod.ts
@@ -0,0 +1,6 @@
+export * from './constants';
+export * from './sync';
+export * from './callbacks';
+
+import * as promises from './promises';
+export { promises };
